83e54e41a7a683a3f1d28c5689fb918250bd0f50
Darshana <darshana.choudhary.000@gmail.com>
2026-04-06 01:37:48 -0700

    Update awsImageUpload.js

---
+++ backend/utils/awsImageUpload.js
@@ -0,0 +1,9 @@
+"use strict";
+const awsConfig = {
+  AWS_ID: "",
+  AWS_SECRET: "",
+  AWS_BUCKET_NAME: "",
+  awsPermission: "",
+};
+
+module.exports = awsConfig;
---
+++ backend/utils/config.js
@@ -0,0 +1,12 @@
+"use strict";
+const config = {
+	secret: "cmpe273_kafka_passport_mongo",
+	frontendURI: "http://localhost:3000",
+	kafkaURI: "localhost:2181",
+	mysqlUser: "",
+	mysqlPassword: "",
+	mysqlHost: "",
+	mysqlDatabase: "",
+};
+
+module.exports = config;
